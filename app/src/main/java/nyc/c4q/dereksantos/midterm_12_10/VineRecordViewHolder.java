3e96cf8c5526a208f7c18c96882c8e2c04e8bf34
sderekj <dereksantos1989@gmail.com>
2016-12-10 17:18:55 -0500

    Fifth Step

--- app/src/main/java/nyc/c4q/dereksantos/midterm_12_10/VineRecordViewHolder.java
+++ app/src/main/java/nyc/c4q/dereksantos/midterm_12_10/VineRecordViewHolder.java
@@ -1,15 +1,20 @@
 package nyc.c4q.dereksantos.midterm_12_10;
 
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
 public class VineRecordViewHolder extends RecyclerView.ViewHolder {
     private final TextView usernameView;
+    private final TextView likedView;
+    private final View view;
 
     public VineRecordViewHolder(View itemView) {
         super(itemView);
         usernameView = (TextView) itemView.findViewById(R.id.vine_username);
+        likedView = (TextView) itemView.findViewById(R.id.vine_liked);
+        view = (View) itemView.findViewById(R.id.vine_view);
     }
 
 
@@ -21,5 +26,8 @@ public class VineRecordViewHolder extends RecyclerView.ViewHolder {
 //        }
 
         usernameView.setText(vineRecord.getUsername());
+        likedView.setText(vineRecord.getLiked());
+//        itemView.setBackgroundColor(Color.parseColor(vineRecord.getProfileBackground()));
+//        itemView.setBackgroundColor(5276389);
     }
 }
